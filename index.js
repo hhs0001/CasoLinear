e35501c89c190e5ce74b8ea603198c2fd78cc437
Heitor Stein <heitorstein@gmail.com>
2021-11-09 22:29:07 -0300

    Atualização MMQ

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ console.clear();
 term.cyan('Seja bem-vindo(a) ao propragrama para solucionar \n');
 term.cyan('A função ajustadora\n');
 term.green('Selecione seu método:\n');
-const itens1 = [`a. Método Linear`]
+const itens1 = [`a. Método Linear`, `b. Método Mínimos quadrados`]
 term.singleColumnMenu(itens1 , function( error , response ) {
 	if(response.selectedIndex === 0){
         console.clear();
-        valoresX = prompt("Digite os valores de X separados por ; ")
+        let valoresX = prompt("Digite os valores de X separados por ; ")
         valoresX = splitString(valoresX, ";")
         let valoresY = prompt("Digite os valores de Y separados por ; ")
         valoresY = splitString(valoresY, ";")
@@ -19,6 +19,20 @@ term.singleColumnMenu(itens1 , function( error , response ) {
         }
         let solucao = metodos.metodoLinear(valoresX, valoresY)
         console.log(`A equação é:\nY = ${solucao.n} + (${solucao.m}X)`)
+        process.exit()
+    } else if(response.selectedIndex === 1){
+        console.clear();
+        let valoresX = prompt("Digite os valores de X separados por ; ")
+        valoresX = splitString(valoresX, ";")
+        let valoresY = prompt("Digite os valores de Y separados por ; ")
+        valoresY = splitString(valoresY, ";")
+        for (let index = 0; index < valoresX.length; index++) {
+            valoresX[index] = Number(valoresX[index])
+            valoresY[index] = Number(valoresY[index])
+        }
+        let solucao = metodos.MMQ(valoresX, valoresY)
+        console.log(`Valores da solução para X: ${solucao[0]}\n`)
+        console.log(`Valores da solução para Y: ${solucao[1]}\n`)
         process.exit()
     }
 });
--- metodos.js
+++ metodos.js
@@ -1,5 +1,9 @@
 module.exports.metodoLinear = (arrayX, arrayY) => {
     
+    if (arrayX.length != arrayY.length) {
+        throw new Error('Os valores de X e Y devem ter o mesmo tamanho!');
+    }
+
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
 
     let somaX = arrayX.reduce(reducer)
@@ -41,4 +45,43 @@ module.exports.metodoLinear = (arrayX, arrayY) => {
         n: n,
         m: m
     }
+}
+
+module.exports.MMQ = (X,Y) => {
+    let x_sum = 0;
+    let y_sum = 0;
+    let xy_sum = 0;
+    let xx_sum = 0;
+    let count = 0;
+    let x = 0;
+    let y = 0;
+    let values_length = X.length;
+
+    if (values_length != Y.length) {
+        throw new Error('Os valores de X e Y devem ter o mesmo tamanho!');
+    }
+    if (values_length === 0) {
+        return [ [], [] ];
+    }
+    for (let i = 0; i< values_length; i++) {
+        x = X[i];
+        y = Y[i];
+        x_sum+= x;
+        y_sum+= y;
+        xx_sum += x*x;
+        xy_sum += x*y;
+        count++;
+    }
+    let m = (count*xy_sum - x_sum*y_sum) / (count*xx_sum - x_sum*x_sum);
+    let b = (y_sum/count) - (m*x_sum)/count;
+    let result_values_x = [];
+    let result_values_y = [];
+    
+    for (let i = 0; i < values_length; i++) {
+        x = X[i];
+        y = x * m + b;
+        result_values_x.push(x);
+        result_values_y.push(y);
+    }
+    return [result_values_x, result_values_y];
 }
